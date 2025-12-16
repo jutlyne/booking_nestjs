fc10b5175b21874d6b80d6010de6b6fad4741dee
NTA_KyVC <kyvc@mail.nitrotech.asia>
2025-12-16 09:08:55 +0700

    add role

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 import { DatabaseConfig } from './config.interface';
 
-export default registerAs<DatabaseConfig>('database', () => {
+export default registerAs<DatabaseConfig>('databases', () => {
   return {
     url: process.env.DATABASE_URL,
   };
