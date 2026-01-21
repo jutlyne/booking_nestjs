fa5e025d92cf900de3ef39644b51614609052add
NTA_KyVC <kyvc@mail.nitrotech.asia>
2026-01-21 10:20:52 +0700

    update

--- src/common/configs/auth.config.ts
+++ src/common/configs/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from './config.interface';
 
 export default registerAs<AuthConfig>('auth', () => {
   return {
-    secret: process.env.AUTH_JWT_SECRET || '123',
+    secret: process.env.AUTH_JWT_SECRET,
     expires: process.env.AUTH_JWT_TOKEN_EXPIRES_IN,
     refreshSecret: process.env.AUTH_REFRESH_SECRET,
     refreshExpires: process.env.AUTH_REFRESH_TOKEN_EXPIRES_IN,
